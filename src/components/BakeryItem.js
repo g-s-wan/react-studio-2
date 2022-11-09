544c87b00e7e490dbae5594bed8ab4f69276ed1a
Grace Wan <grace_wan@brown.edu>
2022-11-09 12:08:54 -0500

    Done?

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,18 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+  const item = props.item;
+  const updateCart = props.updateCart;
+  const cart = props.cart;
+
+  const handleClick = () => {
+    updateCart([...cart, item]);
+  }
+
+  return (
+    <div>
+      <h3>{item.name}</h3>
+      <p>{item.description}</p>
+      <button onClick={handleClick}>Add to Cart</button>
+    </div>
+  )
+}
